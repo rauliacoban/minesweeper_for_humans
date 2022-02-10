a149528dd50a36689328d120305c9b7896e99e57
rauliacoban <iacoban.raul08@gmail.com>
2022-02-10 22:01:08 +0200

    minor changes for PEP8 conformity

--- cell.py
+++ cell.py
@@ -1,12 +1,9 @@
 from tkinter import Button
-import random
 import settings
 from utils import *
-import ctypes
 
 
 class Cell:
-
     def __init__(self, x, y, game):
         self.ismine = False
         self.value = 0
@@ -14,7 +11,7 @@ class Cell:
         self.coords = (x, y)
         self.flagged = False
         self.covered = True
-        self.game=game
+        self.game = game
 
     def create_button(self, location):
         button = Button(location,
@@ -23,7 +20,7 @@ class Cell:
                         height=1,
                         bg='grey',
                         font='sans 9 bold'
-        )
+                        )
         button.bind('<Button-1>', self.left_click)
         button.bind('<Button-3>', self.right_click)
         self.button = button
@@ -47,34 +44,32 @@ class Cell:
             self.game.randomize(self)
             self.game.started = True
 
-        if (self.ismine):
+        if self.ismine:
             self.button.config(bg='magenta', text='!')
             self.game.game_over()
             return
-        self.setText()
+        self.set_text()
 
         if (self.covered and self.value == 0) or (not self.covered and event):
             self.covered = False
             self.uncover_neighbors()
         if self.covered:
-             self.covered = False
-
+            self.covered = False
 
     def right_click(self, event):
-        if not self.covered:
+        if not self.covered or self.game.is_game_over:
             return
 
-        if(self.flagged):
+        if self.flagged:
             self.button.config(bg='grey')
         else:
             self.button.config(bg='black')
         self.flagged = not self.flagged
 
-    def setText(self):
+    def set_text(self):
         if self.ismine:
             self.button.config(text='!', bg='white')
         else:
-            print(self.value)
             self.button.config(text=f'{number_coding[self.value]}', fg=f'{color_coding[self.value]}', bg='white')
 
     def get_neighbors(self):
@@ -83,5 +78,4 @@ class Cell:
             coords = add_coords(self.coords, direction)
             if valid(coords):
                 neighbors.append(self.game.get_cell(coords))
-                #print(f'what get_neighbors receives:{neighbors[-1].coords}')
         return neighbors
